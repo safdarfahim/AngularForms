170fc42a1df2457646bbc2d81c1086c4da8bb77a
safdar fahim <safdarfahim@gmail.com>
2020-04-15 12:35:34 +0530

    chnages in the form

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+defaultQuestion='pet';
+answer='';
+
+genders=['male','female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    }
+    );
   }
+ @ViewChild('f') signupForm:NgForm;
+  // onSubmit( form:NgForm){
+  //   debugger;
+  //   console.log(form);
+  // }
+
+  onSubmit( form:NgForm){
+    console.log(this.signupForm);
+  }
+
 }
